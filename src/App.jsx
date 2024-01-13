92b8f2b8e859f15174e3442ad46467ad327deca1
Anton Boldyrev <boldyrev.webdev@gmail.com>
2024-01-13 16:55:54 +0500

    feat: add initial markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-function App() {
-  const [count, setCount] = useState(0)
+import Header from "./components/Header/Header";
 
+function App() {
   return (
     <>
-
+      <Header />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header__logo">CleanSpaces</div>
+      <ul className="nav">
+        <li className="nav__item">Home</li>
+        <li className="nav__item">About Us</li>
+        <li className="nav__item">Services</li>
+        <li className="nav__item">Careers</li>
+        <li className="nav__item">Contact</li>
+      </ul>
+    </header>
+  );
+}
