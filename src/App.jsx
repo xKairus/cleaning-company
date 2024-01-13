4ca89208682c0031f6b8e5f584b4df62250f5615
Anton Boldyrev <boldyrev.webdev@gmail.com>
2024-01-13 17:34:04 +0500

    dev: add page change functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import Header from "./components/Header/Header";
 
+// Pages
+import HomePage from "./components/HomePage/HomePage";
+import AboutUsPage from "./components/AboutUsPage/AboutUsPage";
+import ServicesPage from "./components/ServicesPage/ServicesPage";
+import CareersPage from "./components/CareersPage/CareersPage";
+import ContactPage from "./components/ContactPage/ContactPage";
+
 function App() {
+  const [page, setPage] = useState("home");
+
   return (
     <>
-      <Header />
+      <Header active={page} onChange={(current) => setPage(current)}/>
+
+      {page === "home" && <HomePage />}
+      {page === "about" && <AboutUsPage />}
+      {page === "services" && <ServicesPage />}
+      {page === "careers" && <CareersPage />}
+      {page === "contact" && <ContactPage />}
     </>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,45 @@
 import classes from "./Header.module.css";
 
-export default function Header() {
+export default function Header({ active, onChange }) {
   return (
     <header className={classes.header}>
       <div className={classes.header__logo}>CleanSpaces</div>
       <ul className={classes.nav}>
-        <li className={classes.nav}>Home</li>
-        <li className={classes.nav}>About Us</li>
-        <li className={classes.nav}>Services</li>
-        <li className={classes.nav}>Careers</li>
-        <li className={classes.nav}>Contact</li>
+        <li
+          className={classes.nav}
+          isActive={active === "home"}
+          onClick={() => onChange("home")}
+        >
+          Home
+        </li>
+        <li
+          className={classes.nav}
+          isActive={active === "about"}
+          onClick={() => onChange("about")}
+        >
+          About Us
+        </li>
+        <li
+          className={classes.nav}
+          isActive={active === "services"}
+          onClick={() => onChange("services")}
+        >
+          Services
+        </li>
+        <li
+          className={classes.nav}
+          isActive={active === "careers"}
+          onClick={() => onChange("careers")}
+        >
+          Careers
+        </li>
+        <li
+          className={classes.nav}
+          isActive={active === "contact"}
+          onClick={() => onChange("contact")}
+        >
+          Contact
+        </li>
       </ul>
     </header>
   );
